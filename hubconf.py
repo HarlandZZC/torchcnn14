da3f7fa54d0cefc0460d97068de74a8ca78f506c
HalrandZZC <harlandzzc@gmail.com>
2024-09-26 19:58:16 +0800

    2024-0926-1958

--- hubconf.py
+++ hubconf.py
@@ -3,10 +3,10 @@ dependencies = ['torch', 'numpy', 'resampy', 'soundfile']
 from torchcnn14.cnn14 import cnn14
 
 model_urls = {
-    'cnn14_16k': 'https://github.com/HarlandZZC/cnn14_torch/'
-              'releases/download/v0.1/Cnn14_16k_mAP=0.438.pth',
-    'cnn14_32k': 'https://github.com/harritaylor/cnn14_torch/'
-           'releases/download/v0.1/Cnn14_mAP=0.431.pth'
+    'cnn14_16k': 'https://github.com/HarlandZZC/torchcnn14/'
+              'releases/download/v0.1/cnn14_16k.pth',
+    'cnn14_32k': 'https://github.com/HarlandZZC/torchcnn14/'
+           'releases/download/v0.1/cnn14_32k.pth'
 }
 
 def vggish(**kwargs):
